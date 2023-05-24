a0d97a9a224f44e65322d66a533774e9df5502bc
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-05-24 23:17:56 +0300

    я тупик искал ошибку 100500 лет дайте нормальные глаза

--- app.js
+++ app.js
@@ -1,45 +1,66 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const usersRoutes = require("./routes/users");
-const cardsRoutes = require("./routes/cards");
-const { NOT_FOUND } = require("./errors/errors_constants");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { errors, celebrate, Joi } = require('celebrate');
+const usersRoutes = require('./routes/users');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const cardsRoutes = require('./routes/cards');
+const { INTERNAL_SERVER_ERROR, NOT_FOUND } = require('./errors/errors_constants');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.set("strictQuery", true);
+mongoose.set('strictQuery', true);
 mongoose
-  .connect("mongodb://0.0.0.0:27017/mestodb")
+  .connect('mongodb://0.0.0.0:27017/mestodb')
   .then(() => {
-    if (process.env.NODE_ENV !== "test") {
-      console.log("Database connected.");
-    }
+    console.log('Database connected.');
   })
   .catch((err) => {
-    console.log("Error on database connection");
-    if (err) {
-      console.error(err);
-    }
+    console.error(err);
   });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "645c1edd4027dafa5a0bb4e2", // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), login);
+
+app.use('/', auth, usersRoutes);
+app.use('/', auth, cardsRoutes);
+
+app.use(errors());
+
+app.use(
+  (req, res) => {
+    res.status(NOT_FOUND).send({ message: 'Неправильный путь' });
+  },
+);
+
+app.use((err, req, res, next) => {
+  if (err.statusCode) {
+    res.status(err.statusCode).send({ message: err.message });
+  } else {
+    res.status(INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка' });
+  }
   next();
-});
-
-app.use(usersRoutes);
-app.use(cardsRoutes);
-
-app.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: "Неправильный путь" });
 });
 
 app.listen(PORT);
