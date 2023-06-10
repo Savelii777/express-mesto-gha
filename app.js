7fe57f00b73201bfbbe73f7289056395932daba7
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-06-10 13:09:41 +0300

    ревью2

--- app.js
+++ app.js
@@ -27,7 +27,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-
+app.get('/crash-test', () => {
+  // краш-тест
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
